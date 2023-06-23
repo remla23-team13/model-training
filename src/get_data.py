7e2cacf35ba1ad274aca219e23ea7e9517de0860
gystemd <gystemd@gmail.com>
2023-06-23 10:11:28 +0200

    linting

--- src/get_data.py
+++ src/get_data.py
@@ -4,8 +4,6 @@ import gdown
 URL = "https://drive.google.com/uc?id=1VNdztX_xMdfvgAdTdOpVJfHt84YcQxn1&export=download"
 OUTPUT = "data/RestaurantReviews.tsv"
 
-def main():
-    gdown.download(URL, OUTPUT, quiet=False)
 
 if __name__ == "__main__":
-    main()
+    gdown.download(URL, OUTPUT, quiet=False)
--- tests/test_non_determinism.py
+++ tests/test_non_determinism.py
@@ -1,24 +1,21 @@
-import pytest
+"""Different tests"""
 import json
+
 import train as training
 
-@pytest.fixture
-def trained_model():
-    from joblib import load
-    yield load('models/model.joblib')
 
-def test_nondeterminism_robustness(trained_model):
+def test_nondeterminism_robustness() -> None:
+    """Train the model with different seeds and check that the accuracy
+    is within 5% of the original"""
     model_accuracy = None
-    with open('metrics/metrics.json') as json_file:
+    with open("metrics/metrics.json", encoding="utf-8") as json_file:
         metrics = json.load(json_file)
-        model_accuracy = metrics['accuracy']
-    X,y = training.load_preprocessed_data('data/preprocessed_data.joblib')
+        model_accuracy = metrics["accuracy"]
+    X, y = training.load_preprocessed_data("data/preprocessed_data.joblib")
 
     for i in range(20):
         X_train, X_test, y_train, y_test = training.create_split(X, y, random_state=i)
         trained_model = training.train(X_train, y_train)
         metrics = training.test(X_test, y_test, trained_model)
-        model_accuracy_ = metrics['accuracy']
-        assert(abs(model_accuracy - model_accuracy_) < 0.05)
-
-
+        model_accuracy_ = metrics["accuracy"]
+        assert abs(model_accuracy - model_accuracy_) < 0.05
