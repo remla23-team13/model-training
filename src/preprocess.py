f1a280f9ef9499ccd9062e14e76dfa95953141ec
gystemd <gystemd@gmail.com>
2023-06-23 18:01:34 +0200

    remove unused imports in preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,20 +1,17 @@
 """Preprocessing phase of the model training pipeline"""
-import re
 from typing import Any
 
-import nltk
 import pandas as pd
 from joblib import dump
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-from sklearn.feature_extraction.text import CountVectorizer
 from remlalib.preprocess import Preprocess
 
 preprocessor = Preprocess()
 
+
 def load_dataset(data_path: str) -> pd.DataFrame:
     """Load dataset from data_path"""
     return pd.read_csv(data_path, delimiter="\t", quoting=3)
+
 
 def preprocess(dataset: pd.DataFrame) -> tuple[Any, Any]:
     """Preprocess the dataset and save it"""
