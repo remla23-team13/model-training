5a9375863b6ad86a61312b479dc40a285c355231
gystemd <gystemd@gmail.com>
2023-06-25 11:42:31 +0200

    cookiecutter-data-science format

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -2,7 +2,7 @@
 from urllib.request import urlopen
 
 URL = "https://drive.google.com/uc?id=1VNdztX_xMdfvgAdTdOpVJfHt84YcQxn1&export=download"
-OUTPUT = "data/RestaurantReviews.tsv"
+OUTPUT = "data/raw/RestaurantReviews.tsv"
 
 if __name__ == "__main__":
     with urlopen(URL) as file:
