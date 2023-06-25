31c66ed97fa298dc54f09d9b39e0494e02b93b33
gystemd <gystemd@gmail.com>
2023-06-25 13:28:00 +0200

    add test for get_data

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -4,7 +4,13 @@ from urllib.request import urlopen
 URL = "https://drive.google.com/uc?id=1VNdztX_xMdfvgAdTdOpVJfHt84YcQxn1&export=download"
 OUTPUT = "data/raw/RestaurantReviews.tsv"
 
-if __name__ == "__main__":
+
+def get_data() -> None:
+    """Download the dataset from remote location"""
     with urlopen(URL) as file:
         with open(OUTPUT, "wb") as out:
             out.write(file.read())
+
+
+if __name__ == "__main__":
+    get_data()
